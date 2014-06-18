a46c9451895364b68e993cfd3c924cbe18cf162d
Ka Che <ch.kirtika@gmail.com>
2014-06-18 14:49:00 +0100

    initial commit

---
+++ Openfile.py
@@ -0,0 +1,23 @@
+__author__ = 'kc'
+
+
+rawinput = '../data/fb_friends_raw.txt'
+
+
+
+
+def getfriends(inputFile):
+    i = 0
+    file = open(inputFile, 'r')
+    for line in file:
+        #if len(line) >1 :
+        #if line.strip():
+         #   if 'friend' not in line.strip().lower():
+        if ((line.strip()) and ('friend' not in line.strip().lower()) and ('message' not in line.strip().lower())):
+            i= i+1
+            print line.strip()
+
+    print 'Number of friends you have',i
+
+
+getfriends(rawinput)
