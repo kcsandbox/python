baee6e502dc4679449975be29eb11c106ddc74c6
Ka Che <ch.kirtika@gmail.com>
2014-06-18 14:51:35 +0100

    initial commit

---
+++ Function.py
@@ -0,0 +1,42 @@
+__author__ = 'kc'
+
+#def Greetings():
+ #   print 'Hello World!'
+
+# Greetings()
+
+#def echo(n):
+ #   print n
+
+#echo(8)
+
+#def echo_double(n):
+ #   print n * 2
+
+#echo_double(16)
+
+
+def double(n):
+     return n * 2
+
+
+#print double(4)
+
+def multiply(x,y):
+    return x*y
+
+a = multiply(27,7)
+#print a
+
+def getname(firstname,lastname):
+    print 'skdjfdkjk'
+    return firstname+' '+lastname
+
+#print getname('Joe','Bloggs')
+
+def copy(inputfile,outfile):
+    print inputfile
+    print outfile
+
+
+copy('myinputfile','myoutputfile')
---
+++ Lists.py
@@ -0,0 +1,26 @@
+__author__ = 'kc'
+myList=[1,24,36,148,50,62]
+#for e in myList:
+for i in range(len(myList)):
+    print myList[i]
+
+for i in range(6):
+    print myList[i]
+
+
+#myList[1]=8
+#print myList[1]
+#myList[5]=100
+#print myList[5]
+
+#print myList[2:5]
+#print myList[2:]
+#print myList[:5]
+#print myList[:]
+
+#print myList[1:2]
+
+
+
+
+
